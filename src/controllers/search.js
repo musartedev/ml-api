4d3208cdbf4eeb32a97ae04b3d6bba523a282440
Mariangélica Useche <mariangelica.useche@gmail.com>
2020-10-11 15:18:48 -0500

    Remove unnecessary logs

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -4,11 +4,9 @@ const { okResponse, errorResponse } = require('../utils/response');
 
 exports.find = async (req, res) => {
   const { q } = req.query;
-  console.log('exports.find -> q', q);
 
   try {
     const result = await searchItems(q);
-    console.log('exports.find -> result', result);
     return okResponse(res, 200, result);
   } catch (err) {
     console.log('exports.find -> err', err);
